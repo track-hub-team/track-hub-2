3c5a78abc3da1ec8c895b898e1c6ab434c581725
Germán Sánchez Carmona <german.san.car@gmail.com>
2025-11-08 19:04:22 +0100

    style: standardize string quotes and formatting across notepad module
    Refs: #0

--- app/modules/notepad/forms.py
+++ app/modules/notepad/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
+
 class NotepadForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired(), Length(max=256)])
-    body = TextAreaField('Body', validators=[DataRequired()])
-    submit = SubmitField('Save notepad')
+    title = StringField("Title", validators=[DataRequired(), Length(max=256)])
+    body = TextAreaField("Body", validators=[DataRequired()])
+    submit = SubmitField("Save notepad")
--- app/modules/notepad/repositories.py
+++ app/modules/notepad/repositories.py
@@ -1,6 +1,7 @@
 from app.modules.notepad.models import Notepad
 from core.repositories.BaseRepository import BaseRepository
 
+
 class NotepadRepository(BaseRepository):
     def __init__(self):
         super().__init__(Notepad)
