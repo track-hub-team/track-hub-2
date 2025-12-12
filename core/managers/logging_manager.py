86b1d0cd8e21202f0063d6aad6454f4673e0eb10
Adolfo Borrego <adolfo.bglez@gmail.com>
2025-12-12 16:39:57 +0100

    feat: vagrant levanat fakenodo
    Refs: #28

--- core/managers/logging_manager.py
+++ core/managers/logging_manager.py
@@ -1,3 +1,4 @@
+import os
 import logging
 from logging.handlers import RotatingFileHandler
 
@@ -7,23 +8,24 @@ class LoggingManager:
         self.app = app
 
     def setup_logging(self):
-        # Configure log format
         formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-        # Configure the log file with file rotation
-        file_handler = RotatingFileHandler("app.log", maxBytes=10240, backupCount=10)
-        file_handler.setLevel(logging.ERROR)
-        file_handler.setFormatter(formatter)
+        # Detect if running in Vagrant
+        working_dir = os.environ.get('WORKING_DIR', '')
+        is_vagrant = working_dir == '/vagrant/'
 
-        # Add handler to app logger
-        self.app.logger.addHandler(file_handler)
+        # Only use file logging outside Vagrant
+        if not is_vagrant:
+            file_handler = RotatingFileHandler("app.log", maxBytes=10240, backupCount=10)
+            file_handler.setLevel(logging.ERROR)
+            file_handler.setFormatter(formatter)
+            self.app.logger.addHandler(file_handler)
 
-        # Configure console log if necessary
+        # Console logging
         if self.app.debug:
             stream_handler = logging.StreamHandler()
             stream_handler.setLevel(logging.INFO)
             stream_handler.setFormatter(formatter)
             self.app.logger.addHandler(stream_handler)
 
-        # Set the overall log level
         self.app.logger.setLevel(logging.INFO)
